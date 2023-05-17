5413ce235fe62af3ce2f80ea5fc9a36f9fa7b783
Isaac Moreira Barbosa <izass1312@gmail.com>
2023-05-17 20:27:20 -0300

    Implement MVC to Idea and Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :ideas
+  resources :ideas do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'ideas#index'
 end
